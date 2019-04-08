2637cfb1a2d02d81162d3a8e4f8fea24f9bd93a7
Neketek <neketek@gmail.com>
2019-04-08 22:35:04 +0300

    Added localization

--- compose.py
+++ compose.py
@@ -7,11 +7,14 @@ env = Environment(
     autoescape=select_autoescape(["html"])
 )
 
+
 main = env.get_template("body.html")
 
 with open("data.yml", "r") as datastream:
     data = yaml.load(datastream)['cv']
 
+with open("loc/{}.yml".format(data.get("loc", "en")), "r") as loc:
+    data['loc'] = yaml.load(loc)['loc']
 
 with open("build/index.html", "w") as f:
     f.write(main.render(**data))
