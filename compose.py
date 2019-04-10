ca1f33ae1055f449e4e0371a951655a29a923d63
Neketek <neketek@gmail.com>
2019-04-10 19:35:01 +0300

    Improved interface for height adjustment

--- compose.py
+++ compose.py
@@ -1,9 +1,14 @@
 from jinja2 import Environment, select_autoescape, FileSystemLoader
 import os
 import yaml
+import pdfkit
 
-DATA_DIR = "./data"
-LOC_DIR = "./loc"
+
+HTML_IN_FILE = os.path.realpath("./build/index.html")
+PDF_OUT_FILE = os.path.realpath("./cv.pdf")
+DATA_DIR = os.path.abspath("./data")
+LOC_DIR = os.path.abspath("./loc")
+PHOTO_DIR = os.path.abspath("./data/photo")
 DEFAULT_LOC = "en.yml"
 
 input_prompt = "Select data file:\n"
@@ -15,13 +20,18 @@ for dirpath, dirnames, filenames in os.walk(DATA_DIR):
         input_prompt += "{}) {}\n".format(i, filename)
         i += 1
     break
-while True:
-    try:
-        selected = int(input(input_prompt))
-        data_filename = input_filenames[selected-1]
-    except (IndexError, ValueError):
-        continue
-    break
+if len(input_filenames) == 1:
+    data_filename = input_filenames[0]
+elif len(input_filenames) == 0:
+    exit("CV data dir has no files!")
+else:
+    while True:
+        try:
+            selected = int(input(input_prompt))
+            data_filename = input_filenames[selected-1]
+        except (IndexError, ValueError):
+            continue
+        break
 
 env = Environment(
     loader=FileSystemLoader("templates"),
@@ -37,18 +47,20 @@ def sort_skills(item):
 
 
 with open(os.path.join(DATA_DIR, data_filename), "r") as datastream:
-    data = yaml.load(datastream)['cv']
+    data = yaml.safe_load(datastream)['cv']
 
 locale_filename = "{}.yml".format(data.get("loc", "en"))
 
 try:
     with open(os.path.join(LOC_DIR, locale_filename), "r") as loc:
-        data['loc'] = yaml.load(loc)['loc']
+        data['loc'] = yaml.safe_load(loc, )['loc']
 except FileNotFoundError:
     with open(os.path.join(LOC_DIR, DEFAULT_LOC)) as loc:
-        data['loc'] = yaml.load(loc)['loc']
+        data['loc'] = yaml.safe_load(loc)['loc']
 
-
+if data.get('photo'):
+    data['photo'] = os.path.join(PHOTO_DIR, data['photo'])
+input_prompt
 if data.get("sort") and data.get("sort").get("skills"):
     data["skills"]["general"].sort(key=sort_skills, reverse=True)
     data["skills"]["technical"].sort(key=sort_skills, reverse=True)
@@ -56,5 +68,22 @@ if data.get("sort") and data.get("sort").get("skills"):
 
 with open("build/index.html", "w") as f:
     f.write(main.render(**data))
+
+options = {
+    'page-width': '26cm',
+    'margin-top': '0',
+    'margin-right': '0',
+    'margin-bottom': '0',
+    'margin-left': '0',
+    'encoding': "UTF-8",
+    'no-outline': None
+}
+
+
+options['page-height'] = "{}cm".format(
+    int(input("Enter page height in cm:\n"))
+)
+
+pdfkit.from_file(HTML_IN_FILE, PDF_OUT_FILE, options=options)
 
 print("Template rendered successfuly")
