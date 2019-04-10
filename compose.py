558dd6c06c81428931b2c1ed199dc7ed5fa2205d
Neketek <neketek@gmail.com>
2019-04-10 16:55:54 +0300

    Minor interface improvements + ru loc

--- compose.py
+++ compose.py
@@ -1,6 +1,27 @@
 from jinja2 import Environment, select_autoescape, FileSystemLoader
+import os
 import yaml
 
+DATA_DIR = "./data"
+LOC_DIR = "./loc"
+DEFAULT_LOC = "en.yml"
+
+input_prompt = "Select data file:\n"
+input_filenames = None
+for dirpath, dirnames, filenames in os.walk(DATA_DIR):
+    i = 1
+    input_filenames = filenames
+    for filename in filenames:
+        input_prompt += "{}) {}\n".format(i, filename)
+        i += 1
+    break
+while True:
+    try:
+        selected = int(input(input_prompt))
+        data_filename = input_filenames[selected-1]
+    except (IndexError, ValueError):
+        continue
+    break
 
 env = Environment(
     loader=FileSystemLoader("templates"),
@@ -10,11 +31,30 @@ env = Environment(
 
 main = env.get_template("body.html")
 
-with open("data.yml", "r") as datastream:
+
+def sort_skills(item):
+    return "{}{}".format(item['value'], item["label"])
+
+
+with open(os.path.join(DATA_DIR, data_filename), "r") as datastream:
     data = yaml.load(datastream)['cv']
 
-with open("loc/{}.yml".format(data.get("loc", "en")), "r") as loc:
-    data['loc'] = yaml.load(loc)['loc']
+locale_filename = "{}.yml".format(data.get("loc", "en"))
+
+try:
+    with open(os.path.join(LOC_DIR, locale_filename), "r") as loc:
+        data['loc'] = yaml.load(loc)['loc']
+except FileNotFoundError:
+    with open(os.path.join(LOC_DIR, DEFAULT_LOC)) as loc:
+        data['loc'] = yaml.load(loc)['loc']
+
+
+if data.get("sort") and data.get("sort").get("skills"):
+    data["skills"]["general"].sort(key=sort_skills, reverse=True)
+    data["skills"]["technical"].sort(key=sort_skills, reverse=True)
+
 
 with open("build/index.html", "w") as f:
     f.write(main.render(**data))
+
+print("Template rendered successfuly")
