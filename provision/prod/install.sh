e44d85bc480b58c566103ac102ebd67c358fdedc
Neketek <neketek@gmail.com>
2019-04-13 23:43:01 +0300

    Finnaly! Everything is working as expected!

--- provision/prod/install.sh
+++ provision/prod/install.sh
@@ -1,9 +1,19 @@
 apt-get update
 DEBIAN_FRONTEND=noninteractive
-apt-get install -y python3 python3-pip nodejs npm xvfb libfontconfig wkhtmltopdf
+apt-get install -y python3 python3-pip nodejs npm wget xvfb
+
+
+wget "https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb"
+apt install -y ./wkhtmltox_0.12.5-1.bionic_amd64.deb
+rm ./wkhtmltox_0.12.5-1.bionic_amd64.deb
+
+
 npm install -g npx
 pip3 install -r requirements.txt
 npm install
-printf '#!/bin/bash\nxvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf -q $*' > /usr/bin/wkhtmltopdf.sh
+
+
+printf '#!/bin/bash\nxvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/local/bin/wkhtmltopdf-orig -q $*' > /usr/bin/wkhtmltopdf.sh
 chmod a+x /usr/bin/wkhtmltopdf.sh
+mv /usr/local/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf-orig
 ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
